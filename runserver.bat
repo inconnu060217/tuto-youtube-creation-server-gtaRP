5c13eb84a3451e0c61d2529d04347d5e01eb0dad
inconnu060217 <jean-francois.traore@epitech.eu>
2023-07-17 00:57:58 +0200

    [INIT]projet gta rp

---
+++ runserver.bat
@@ -0,0 +1,4 @@
+@echo OFF
+RMDIR /s /q "E:\tuto-youtube-creation-server-gtaRP\cache\files"
+cd /d E:\tuto-youtube-creation-server-gtaRP
+E:\tuto-youtube-creation-server-gtaRP\FXServer.exe +exec server.cfg
